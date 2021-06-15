5a33f7f33d1dd8d4b8546143da8b94adbee81b9c
marcoskloss <marcoskloss2001@gmail.com>
2021-06-15 20:21:26 -0300

    add sidebar drawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 import { Logo } from "./Logo";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+  
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+  
   return (
     <Flex
       as='header'
@@ -17,15 +26,27 @@ export function Header() {
       px='6'
       align='center'
     >
+      {!isWideVersion && (
+        <IconButton 
+          aria-label='Abrir menu de navegação'
+          icon={<Icon as={RiMenuLine} />}
+          variant='unstyled'
+          fontSize='24'
+          onClick={onOpen}
+          mr='2'
+        />
+      )}
+      
       <Logo />
-      <SearchBox />
+      
+      { isWideVersion && <SearchBox /> }
 
       <Flex
         align='center'
         ml='auto'
       >
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,23 +1,44 @@
-import { Box, Stack } from "@chakra-ui/react";
-import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
-
-import { NavSection } from './NavSection';
-import { NavLink } from './NavLink';
+import { 
+  Box, 
+  Drawer, 
+  useBreakpointValue, 
+  DrawerContent,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody
+} from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
+  const { isOpen, onClose } = useSidebarDrawer();
+  
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false
+  });
+
+  if (isDrawerSidebar) {
+    return (
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent bg='gray.800'>
+            <DrawerCloseButton mt='2' />
+            <DrawerHeader>Navegação</DrawerHeader>
+
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
+  }
+  
   return (
     <Box as='aside' w='64' mr='8'>
-      <Stack spacing='12' align='flex-start'>
-        <NavSection title='geral'>
-          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-          <NavLink icon={RiContactsLine}>Usuários</NavLink>
-        </NavSection>
-
-        <NavSection title='automação'>
-          <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
-        </NavSection>
-      </Stack>
+      <SidebarNav />
     </Box>
   );
 }
