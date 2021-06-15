33f21a1b997cd97cf56ae86160f41cc1f3777ced
marcoskloss <marcoskloss2001@gmail.com>
2021-06-15 20:21:04 -0300

    add responsive layout

--- src/components/Header/NotificationsNav.tsx
+++ src/components/Header/NotificationsNav.tsx
@@ -3,23 +3,17 @@ import { RiNotificationLine, RiUserLine } from 'react-icons/ri'
 
 export function NotificationsNav() {
   return (
-    <Flex
-      align='center'
-      ml='auto'
+    <HStack 
+      spacing={['6', '8']}
+      pr={['6', '8']}
+      mx={['6', '8']}
+      borderRightWidth={1}
+      borderColor='gray.700'
+      color='gray.300'
+      py='1'
     >
-      <HStack 
-        spacing='4'
-        borderRightWidth={1}
-        borderColor='gray.700'
-        color='gray.300'
-        mx='8'
-        pr='8'
-        py='1'
-      >
-        <Icon as={RiNotificationLine} />
-        <Icon as={RiUserLine} />
-      </HStack>
-
-    </Flex>
-  )
+      <Icon as={RiNotificationLine} />
+      <Icon as={RiUserLine} />
+    </HStack>
+  );
 }
